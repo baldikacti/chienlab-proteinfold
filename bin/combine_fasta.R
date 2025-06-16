7c68a413c03bb49ffe180260812c1c6b1df3c4e8
baldikacti <baldikacti@umass.com>
2025-06-15 23:48:19 -0400

    Modified combine_fasta.R. Now checks for absolute path. Accepts fasta files in acclist. Accepts fasta files with multiple entries in acclist.

--- bin/combine_fasta.R
+++ bin/combine_fasta.R
@@ -2,87 +2,138 @@
 library(optparse)
 
 option_list <- list(
-  make_option(c("--acc_file"),
-    type = "character", default = NULL,
+  make_option(
+    c("--acc_file"),
+    type = "character",
+    default = NULL,
     help = "Path to tsv file with accession numbers.\n Headers: geneID, bait"
   ),
-  make_option(c("--proj_dir"),
-    type = "character", default = NULL,
+  make_option(
+    c("--proj_dir"),
+    type = "character",
+    default = NULL,
     help = "Path to project directory"
   )
 )
 
 opt <- parse_args(OptionParser(option_list = option_list))
 
+# Pull rest of the libraries
 library(seqinr)
-getFasta <- function(base_url, outfile) {
-  library(httr2)
-  pb <- progress::progress_bar$new(
-    format = "Downloading Fasta [:bar] :percent",
-    total = length(base_url),
-    clear = FALSE, width = 60
+library(httr2)
+
+get_fasta <- function(base_url, header, bait_status) {
+  sp <- request(base_url) |>
+    req_perform() |>
+    resp_body_string()
+
+  # Split into lines
+  lines <- unlist(strsplit(sp, "\n"))
+  # Get sequence
+  sequence <- paste(lines[-1], collapse = "")
+
+  # Create dataframe
+  data.frame(
+    fasta_names = header,
+    fasta_sequence = sequence,
+    bait_status = bait_status,
+    stringsAsFactors = FALSE
   )
-  for (url in base_url) {
-    tryCatch(
-      {
-        request(url) |>
-          req_perform() |>
-          resp_body_string() |>
-          write.table(
-            file = paste0(outfile, ".fasta"),
-            quote = F, row.names = F, col.names = F, append = T
-          )
-        message(paste0("Downloaded: ", basename(url), "\n"))
-      },
-      error = function(e) {
-        message(paste("Encountered error processing:", basename(url)))
-        message(conditionMessage(e))
-        NA
-      }
-    )
-
-    Sys.sleep(0.1) # To not spam uniprot
-    pb$tick()
-  }
 }
 
-acclist <- read.table(opt$acc_file, header = T, sep = "\t")
-fasta_files <- grepl("\\.fasta$", acclist[[1]])
-acc_fasta <- acclist[fasta_files, 1]
-acc_fasta_names <- sub(".fasta", "", basename(acc_fasta))
+acclist <- read.table(
+  opt$acc_file,
+  header = TRUE,
+  sep = "\t",
+  stringsAsFactors = FALSE
+)
+# Identify fasta files and uniprot accessions and bait status
+fasta_files <- grepl("\\.(fasta|fa)$", acclist$Entry, ignore.case = TRUE)
+acc_fasta <- acclist$Entry[fasta_files]
+acc_fasta_bait <- acclist$bait[fasta_files]
+acc_uniprot <- acclist$Entry[!fasta_files]
+acc_uniprot_bait <- acclist$bait[!fasta_files]
 
-acc_uniprot <- acclist[!fasta_files, 1]
+# Check if paths are absolute or relative
+is_absolute_path <- function(path) {
+  # On Unix-like systems, absolute paths start with "/"
+  # On Windows, they start with a drive letter (e.g., "C:/") or "\\"
+  grepl("^(/|[A-Za-z]:/|\\\\)", path)
+}
 
-query_url <- sprintf("https://rest.uniprot.org/uniprotkb/%s.fasta", acc_uniprot)
-
-getFasta(query_url, outfile = "query")
-
-# Reads the fasta files and creates combinations of proteins
-fasta <- read.fasta("query.fasta", seqtype = "AA", as.string = TRUE, set.attributes = FALSE) |>
-  setNames(acc_uniprot)
-if (sum(fasta_files) > 0) {
+# Read fasta files into a dataframe with bait status
+fasta_df <- data.frame()
+if (length(acc_fasta) > 0) {
   # Copy files to workdir
-  file.copy(file.path(opt$proj_dir, acc_fasta), ".")
-  fasta_file <- lapply(basename(acc_fasta), \(x) read.fasta(x, seqtype = "AA", as.string = TRUE, set.attributes = FALSE)) |>
-    unlist(recursive = FALSE)
-  fasta <- c(fasta, fasta_file)
-  # Sanitize input names if it contains file paths
-  acclist[fasta_files, 1] <- acc_fasta_names
-  file.remove(basename(acc_fasta))
+  if (is_absolute_path(acc_fasta)) {
+    file.copy(acc_fasta, ".")
+  } else {
+    file.copy(file.path(opt$proj_dir, acc_fasta), ".")
+  }
+  fasta_df <- do.call(
+    rbind,
+    lapply(seq_along(acc_fasta), function(i) {
+      fasta_content <- read.fasta(
+        acc_fasta[i],
+        seqtype = "AA",
+        as.string = TRUE,
+        set.attributes = FALSE
+      )
+      data.frame(
+        fasta_names = names(fasta_content),
+        fasta_sequence = unlist(fasta_content, use.names = FALSE),
+        bait_status = acc_fasta_bait[i],
+        stringsAsFactors = FALSE
+      )
+    })
+  )
 }
+
+# For non-fasta entries, use get_fasta to fetch from Uniprot
+if (length(acc_uniprot) > 0) {
+  uniprot_df <- do.call(
+    rbind,
+    lapply(seq_along(acc_uniprot), function(i) {
+      get_fasta(
+        sprintf("https://rest.uniprot.org/uniprotkb/%s.fasta", acc_uniprot[i]),
+        acc_uniprot[i],
+        acc_uniprot_bait[i]
+      )
+    })
+  )
+}
+
+fasta_df <- rbind(fasta_df, uniprot_df)
 
 # Combine bait-prey pairs
-combs <- expand.grid(acclist[acclist[[2]] == TRUE, 1], acclist[acclist[[2]] == FALSE, 1], stringsAsFactors = FALSE)
+combs <- expand.grid(
+  fasta_df[fasta_df$bait_status == TRUE, "fasta_names"],
+  fasta_df[fasta_df$bait_status == FALSE, "fasta_names"],
+  stringsAsFactors = FALSE
+)
 combs$combined <- paste(combs$Var1, combs$Var2, sep = "-")
-fast_out <- mapply(\(x, y) {
-  paste(fasta[[x]], fasta[[y]], sep = ":")
-}, combs$Var1, combs$Var2, SIMPLIFY = FALSE) |>
+
+# Function to pull sequences from the dataframe based on fasta_names
+pull_sequence <- function(df, fasta_name) {
+  unique(df$fasta_sequence[df$fasta_names == fasta_name])
+}
+
+fasta_out <- mapply(
+  \(x, y) {
+    paste(pull_sequence(fasta_df, x), pull_sequence(fasta_df, y), sep = ":")
+  },
+  combs$Var1,
+  combs$Var2,
+  SIMPLIFY = FALSE
+) |>
   setNames(combs$combined)
 
 # Write pair results to fasta files
-for (i in seq_along(fast_out)) {
-  write.fasta(fast_out[[i]], names = names(fast_out)[i], file.out = paste0(names(fast_out)[i], ".fasta"), as.string = TRUE)
+for (i in seq_along(fasta_out)) {
+  write.fasta(
+    fasta_out[[i]],
+    names = names(fasta_out)[i],
+    file.out = paste0(names(fasta_out)[i], ".fasta"),
+    as.string = TRUE
+  )
 }
-
-# Clean up
-file.remove("query.fasta")
